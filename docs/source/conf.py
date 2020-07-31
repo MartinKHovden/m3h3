715c5ab0fc124ce40fffbb81763de13e51094ae9
martinkh <marthvdn@online.no>
2020-07-31 11:59:41 +0200

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["recommonmark", "sphinx.ext.mathjax", "sphinx.ext.autodoc"
+extensions = ["recommonmark", "sphinx_math_dollar", "sphinx.ext.mathjax", "sphinx.ext.autodoc"
 ,"sphinx.ext.napoleon", "sphinx.ext.coverage"]
 
 mathjax_config = {
