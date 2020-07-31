c893bcdab84308d48459f11b73968d3046e1f872
martinkh <marthvdn@online.no>
2020-07-31 12:48:47 +0200

    Update __init__.py

--- m3h3/ode/__init__.py
+++ m3h3/ode/__init__.py
@@ -2,6 +2,7 @@ from m3h3.ode.cardiac_cell_model import CardiacCellModel, MultiCellModel
 from m3h3.ode.no_cell_model import NoCellModel
 from m3h3.ode.tentusscher_panfilov_2006_M_cell import (
                                             Tentusscher_panfilov_2006_M_cell)
+from m3h3.ode.beeler_reuter_1977 import Beeler_reuter_1977
 
 __all__ = ['CardiacCellModel', 'MultiCellModel', 'NoCellModel',
             'Tentusscher_panfilov_2006_M_cell', 'Beeler_reuter_1977']
